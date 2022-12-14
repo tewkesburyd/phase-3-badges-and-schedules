35ce048750d40e0fe7997363b2f648d02d110982
David Tewkesbury <tewkesburyd@hotmail.com>
2022-12-13 20:08:34 -0700

    commit tree d1cf977e6c16c2a884ca609c81c97ecd7e5d521b

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.collect do |person| badge_maker(person)
+    end
+end
+
+def assign_rooms speakers
+    speakers.collect.each_with_index do |speaker, index|        
+        "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer names
+
+    batch_badge_creator(names).each do |a| puts a
+    end
+
+    assign_rooms(names).each do |b| puts b
+    end
+
+end
